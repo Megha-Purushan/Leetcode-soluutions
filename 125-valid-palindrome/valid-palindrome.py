4d1c14740f9106a40bad56f6d912365f8002e548
Megha-Purushan <meghapurushan88@gmail.com>
2026-06-15 20:10:45 +0530

    Time: 11 ms (33.23%) | Memory: 19.7 MB (42.12%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new=""
+        for i in s:
+            if i.isalnum():
+                new=new+i.lower()
+        if new==new[::-1]:
+            return True
+        else:
+            return False
