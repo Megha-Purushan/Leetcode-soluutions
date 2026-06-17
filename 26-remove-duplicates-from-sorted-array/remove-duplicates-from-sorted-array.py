16ec5f905a5df27814eba9b8dceb79f1b7717d17
Megha-Purushan <meghapurushan88@gmail.com>
2026-06-17 21:44:31 +0530

    Time: 4 ms (41.99%) | Memory: 13.9 MB (13.99%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,7 +3,7 @@ class Solution(object):
         k=1
         for i in range(1,len(nums)):
             if nums[i]!=nums[i-1]:
-                temp=nums[i]
+                
                 nums[k]=nums[i]
                 k=k+1
         return k
